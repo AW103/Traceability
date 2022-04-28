aeb9b3a57660dc860dc0cfa10579228d026e244a
Andrea Walsh <andreawalsh@Carters1MacBook.attlocal.net>
2022-04-28 17:22:57 -0500

    rollbar experiment

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ var rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
+app.use(rollbar.errorHandler())
 
 app.use(express.json());
 
 app.use(express.static("public"));
-app.use(rollbar.errorHandler())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
@@ -28,8 +28,9 @@ app.get("/hello", (req,res) => {
 res.status(200).send("Hello there")
 } catch (err) {
     console.error("Not working")
-    rollbar.critical("Invalid function")
+    rollbar.critical("This is critical err")
     rollbar.info("This is info err")
+    rollbar.warning("This is warning err")
 }
 })
 
